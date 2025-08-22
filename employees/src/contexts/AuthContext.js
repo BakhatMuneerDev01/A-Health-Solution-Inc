0c14fe5d8e15d12a328a4c3e8255578810f6f08b
BakhatMuneerDev01 <bakhatmuneer.webdev@gmail.com>
2025-08-23 00:57:37 +0500

    feat(auth): add authentication context and routing setup
    - Create AuthContext with login/logout functionality and local storage persistence
    - Set up basic routing structure in App component

---
+++ employees/src/contexts/AuthContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useEffect } from 'react';
+import { dummyUsers } from "../utils/dummyUsers";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [isAuthenticated, setIsAuthenticatedd] = useState(false)
+
+    const login = (email, password) => {
+        let foundUser = dummyUsers.find(user => user.email === email && user.password === password)
+        if (foundUser) {
+            setUser(foundUser)
+            setIsAuthenticatedd(true)
+            localStorage.setItem('user', JSON.stringify(foundUser)); // Store user in local storage
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+    const logout = () => {
+        setUser(null)
+        setIsAuthenticatedd(false)
+        localStorage.removeItem('user');
+    }
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+            setIsAuthenticatedd(true);
+        };
+
+    }, [])
+
+    return (
+        <AuthProvider.Provider value={{ user, isAuthenticated, login, logout }}>
+            {children}
+        </AuthProvider.Provider>
+    )
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
